8e77f0ec611c93953e4117ce376d056a6c98b733
Hubert Wawrzacz <hubert.wawrzacz@gmail.com>
2019-06-19 21:29:04 +0200

    Get regex validation done

--- RegexTesting/RegexTest.cs
+++ RegexTesting/RegexTest.cs
@@ -7,17 +7,18 @@ using System.Text.RegularExpressions;
 
 namespace RegexTesting
 {
-    class Program
+    class RegexTest
     {
         static void Main(string[] args)
         {
             while (true)
             {
-                const string NAME_PATTERN = @"([0-9]\.[0-9])";
+                const string MANUFACTURER_PATTERN = @"^[A-z][a-z]*$";
+                const string MODEL_PATTERN = @"^[A-z0-9]+$";
+                const string CAPACITY_PATTERN = @"^[0-9]\.[0-9]$";
                 Console.Write("Type something to test: ");
                 string textToCheck = Console.ReadLine().Trim();
-                Match match = Regex.Match(textToCheck, NAME_PATTERN);
-
+                Match match = Regex.Match(textToCheck, CAPACITY_PATTERN);
                 if (match.Success)
                     Console.WriteLine("Pattern matched");
                 else Console.WriteLine("Patternt not matched");
